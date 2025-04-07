ab368ca52e446125cbde2c3548d52411501c9e04
Aozora9200 <aozora9200@gmail.com>
2025-04-07 20:29:52 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 19
         targetSdk = 35
         versionCode = 1
-        versionName = "1.00-AZB01C"
+        versionName = "1.01-AZB0101"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
